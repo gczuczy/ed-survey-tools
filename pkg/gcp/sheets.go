e1fc8586208e55222e5d823e9b25d4fa1430e2b0
Gergely Czuczy <gergely.czuczy@harmless.hu>
2026-03-11 21:58:05 +0100

    Refactor sheet parsing to be abstracted

--- pkg/gcp/sheets.go
+++ pkg/gcp/sheets.go
@@ -10,15 +10,52 @@ import (
 	"google.golang.org/api/option"
 )
 
+type Sheet interface {
+	GetName() string
+	Get(row, col int) string
+	Rows() int
+}
+
+type GSheet struct {
+	name string
+	data [][]interface{}
+}
+
+func (s *GSheet) GetName() string {
+	return s.name
+}
+
+func (s *GSheet) Rows() int {
+	return len(s.data)
+}
+
+func (s *GSheet) Get(row, col int) string {
+	if row >= len(s.data) {
+		return ""
+	}
+	r := s.data[row]
+	if col >= len(r) {
+		return ""
+	}
+	v := r[col]
+	if v == nil {
+		return ""
+	}
+	if str, ok := v.(string); ok {
+		return str
+	}
+	return fmt.Sprintf("%v", v)
+}
+
 type GSpreadsheetsService struct {
-	Token *oauth2.Token
+	Token         *oauth2.Token
 	SheetsService *sheets.Service
 }
 
 type GSpreadsheet struct {
-	Sheet *sheets.Spreadsheet
+	meta          *sheets.Spreadsheet
 	SheetsService *sheets.Service
-	ID string
+	ID            string
 }
 
 func NewSheets() (*GSpreadsheetsService, error) {
@@ -29,7 +66,8 @@ func NewSheets() (*GSpreadsheetsService, error) {
 
 	scopes := option.WithScopes(sheets.SpreadsheetsScope)
 
-	if gs.SheetsService, err = sheets.NewService(ctx, authOption, scopes); err != nil {
+	if gs.SheetsService, err = sheets.NewService(
+		ctx, authOption, scopes); err != nil {
 		return nil, err
 	}
 
@@ -39,11 +77,11 @@ func NewSheets() (*GSpreadsheetsService, error) {
 func (s *GSpreadsheetsService) Sheet(id string) (*GSpreadsheet, error) {
 	var err error
 	sheet := &GSpreadsheet{
-		ID: id,
+		ID:            id,
 		SheetsService: s.SheetsService,
 	}
 
-	sheet.Sheet, err = s.SheetsService.Spreadsheets.Get(id).Do()
+	sheet.meta, err = s.SheetsService.Spreadsheets.Get(id).Do()
 	if err != nil {
 		return nil, err
 	}
@@ -51,19 +89,45 @@ func (s *GSpreadsheetsService) Sheet(id string) (*GSpreadsheet, error) {
 	return sheet, nil
 }
 
-func (s *GSpreadsheet) GetSheets() []*sheets.Sheet {
-	return s.Sheet.Sheets
+func (s *GSpreadsheet) readSheet(
+	name string) (*sheets.ValueRange, error) {
+	f := func() (*sheets.ValueRange, error) {
+		return s.SheetsService.Spreadsheets.Values.
+			Get(s.ID, name).Do()
+	}
+	return RateLimit(f, 30*time.Second)
 }
 
-func (s *GSpreadsheet) ReadRange(sheet string, start string, end string) (ret *sheets.ValueRange, err error) {
+func (s *GSpreadsheet) GetSheets() ([]Sheet, error) {
+	result := make([]Sheet, 0, len(s.meta.Sheets))
+	for _, sh := range s.meta.Sheets {
+		name := sh.Properties.Title
+		data, err := s.readSheet(name)
+		if err != nil {
+			return nil, errors.Join(err,
+				fmt.Errorf("GetSheets(%s)", name))
+		}
+		result = append(result, &GSheet{
+			name: name,
+			data: data.Values,
+		})
+	}
+	return result, nil
+}
+
+func (s *GSpreadsheet) ReadRange(
+	sheet string, start string, end string,
+) (ret *sheets.ValueRange, err error) {
 	rangestr := fmt.Sprintf("%s!%s:%s", sheet, start, end)
 	f := func() (*sheets.ValueRange, error) {
-		return s.SheetsService.Spreadsheets.Values.Get(s.ID, rangestr).Do()
+		return s.SheetsService.Spreadsheets.Values.
+			Get(s.ID, rangestr).Do()
 	}
 	ret, err = RateLimit(f, 30*time.Second)
 	if err != nil {
 		fmt.Printf("ReadCell error: %T/%v", err, err)
-		err = errors.Join(err, fmt.Errorf("ReadCell(%s!%s:%s)", sheet, start, end))
+		err = errors.Join(err,
+			fmt.Errorf("ReadCell(%s!%s:%s)", sheet, start, end))
 	}
 	return ret, err
 }
--- pkg/vsds/densitysheet.go
+++ pkg/vsds/densitysheet.go
@@ -2,136 +2,70 @@ package vsds
 
 import (
 	"fmt"
-	"time"
 	"errors"
 	"strings"
 	"strconv"
 
-	"google.golang.org/api/sheets/v4"
 	"github.com/gczuczy/ed-survey-tools/pkg/gcp"
 	vsdstypes "github.com/gczuczy/ed-survey-tools/pkg/vsds/types"
 )
 
-const (
-	MaxSamples = 256
-)
-
-type Spreadsheet struct {
-	spreadsheet *gcp.GSpreadsheet
-}
-
-func NewSpreadsheet(sheetid string, ss *gcp.GSpreadsheetsService) (*Spreadsheet, error) {
-	var (
-		s *gcp.GSpreadsheet
-		err error
-	)
-	f := func() (*gcp.GSpreadsheet, error) {
-		return ss.Sheet(sheetid)
-	}
-
-	s, err = gcp.RateLimit(f, 30 * time.Second)
-	if err != nil {
-		return nil, errors.Join(err, fmt.Errorf("Unable to load sheet %s", sheetid))
-	}
-
-	return &Spreadsheet{
-		spreadsheet: s,
-	}, nil
-}
-
-func (ds *Spreadsheet) GetSurveys() ([]vsdstypes.Survey, error) {
-	var reterr error = nil
-	ret := []vsdstypes.Survey{}
-
-	for _, sheet := range ds.spreadsheet.GetSheets() {
-		if m, err := ds.parseSheet(sheet.Properties.Title); err != nil {
-			//fmt.Printf("Sheet errors: %v\n", err)
-			err = errors.Join(reterr, err)
-		} else {
-			ret = append(ret, m)
-		}
-	}
-
-	return ret, reterr
-}
-
-func (ds *Spreadsheet) parseSheet(name string) (vsdstypes.Survey, error) {
+func ParseSheet(sheet gcp.Sheet) (vsdstypes.Survey, error) {
+	name := sheet.GetName()
 	m := vsdstypes.Survey{
-		Name: name,
+		Name:         name,
 		SurveyPoints: make([]vsdstypes.SurveyPoint, 0, 32),
 	}
 
-	endcell := fmt.Sprintf("I%d", MaxSamples)
-
-	// get the cmdrname and project
-	data, err := ds.spreadsheet.ReadRange(name, "A1", endcell)
-	if err != nil {
-		return m, err
-	}
-	parts := strings.Split(data.Values[0][0].(string), " - ")
+	parts := strings.Split(sheet.Get(0, 0), " - ")
 	if len(parts) == 2 {
 		m.CMDR = parts[0]
 		m.Project = parts[1]
 	}
 
-	// identify the sheet type
-	var variant *sheetVariant = nil
+	var variant *sheetVariant
 	for _, sv := range sheetVariants {
-		if evalSheetVariant(sv, data) {
+		if evalSheetVariant(sv, sheet) {
 			variant = sv
 			break
 		}
 	}
 	if variant == nil {
-		//fmt.Printf("Unable to identify sheet variant for %s/%s\n",
-		//	ds.spreadsheet.ID, name)
-		return m, fmt.Errorf("Unable to identify sheet variant for %s/%s",
-			ds.spreadsheet.ID, name)
+		return m, fmt.Errorf(
+			"Unable to identify sheet variant for %s", name)
 	}
 
 	var (
-		z int
-		c int
+		z     int
+		c     int
 		mdstr string
-		md float64
+		md    float64
+		err   error
 	)
-	for i := variant.HeaderRow+1; i < len(data.Values); i += 1 {
-		row := data.Values[i]
-
-		// if the ZSample is empty, bailout, that's the end of the road
-		if len(row) <= variant.ZSampleColumn || row[variant.ZSampleColumn].(string) == "" {
+	for i := variant.HeaderRow + 1; i < sheet.Rows(); i++ {
+		if sheet.Get(i, variant.ZSampleColumn) == "" {
 			break
 		}
-
-		if z, err = strconv.Atoi(row[variant.ZSampleColumn].(string)); err != nil {
-			// skip
+		if z, err = strconv.Atoi(
+			sheet.Get(i, variant.ZSampleColumn)); err != nil {
 			continue
-			//return m, errors.Join(err, fmt.Errorf("Conversion ZSample error '%v': %s/%s",
-			//row[variant.ZSampleColumn], ds.spreadsheet.ID, name))
 		}
-		if c, err = strconv.Atoi(row[variant.SystemCountColumn].(string)); err != nil {
-			// skip
+		if c, err = strconv.Atoi(
+			sheet.Get(i, variant.SystemCountColumn)); err != nil {
 			continue
-			//return m, errors.Join(err, fmt.Errorf("Conversion SysCnt error '%v': %s/%s",
-			//	row[variant.SystemCountColumn], ds.spreadsheet.ID, name))
 		}
-		//fmt.Printf("%s/%s/r%d: %+v\n", ds.spreadsheet.ID, name, i, row)
-		if len(row) <= variant.MaxDistanceColumn {
-			mdstr = ""
-		} else {
-			mdstr = row[variant.MaxDistanceColumn].(string)
-		}
+		mdstr = sheet.Get(i, variant.MaxDistanceColumn)
 		if mdstr == "" {
 			md = 20.0
 		} else if md, err = strconv.ParseFloat(mdstr, 32); err != nil {
-			return m, errors.Join(err, fmt.Errorf("Conversion MaxDst error %d/%d '%v': %s/%s",
-				i, variant.MaxDistanceColumn,
-				row[variant.MaxDistanceColumn], ds.spreadsheet.ID, name))
+			return m, errors.Join(err, fmt.Errorf(
+				"Conversion MaxDst error %d/%d '%v': %s",
+				i, variant.MaxDistanceColumn, mdstr, name))
 		}
 		dp := vsdstypes.SurveyPoint{
-			SystemName: row[variant.SysNameColumn].(string),
-			ZSample: z,
-			Count: c,
+			SystemName:  sheet.Get(i, variant.SysNameColumn),
+			ZSample:     z,
+			Count:       c,
 			MaxDistance: float32(md),
 		}
 		m.SurveyPoints = append(m.SurveyPoints, dp)
@@ -140,62 +74,41 @@ func (ds *Spreadsheet) parseSheet(name string) (vsdstypes.Survey, error) {
 	return m, nil
 }
 
-func evalSheetVariant(sv *sheetVariant, data *sheets.ValueRange) bool {
-
+func evalSheetVariant(sv *sheetVariant, sheet gcp.Sheet) bool {
 	for _, check := range sv.HeaderChecks {
-		if len(data.Values) <= check.Row {
-			//fmt.Printf("(%s) Not enough rows has:%d check:%d\n", sv.Name,
-			//	len(data.Values), check.Row)
+		if sheet.Rows() <= check.Row {
 			return false
 		}
-		if len(data.Values[check.Row]) <= check.Column {
-			//fmt.Printf("(%s) Not enough columns has:%d check:%d\n  %+v\n", sv.Name,
-			//	len(data.Values[check.Row]), check.Column, data.Values[check.Row])
-			return false
-		}
-		value := data.Values[check.Row][check.Column].(string)
-		if value != check.Value {
-			//fmt.Printf("(%s) Does not match: '%s' VS '%s'\n", sv.Name, value, check.Value)
+		if sheet.Get(check.Row, check.Column) != check.Value {
 			return false
 		}
 	}
 
-	// check data validity, system names should be filled in the Z Sample col
 	nsamples := 0
 	nzsamples := 0
-	for i := sv.HeaderRow+1; i < len(data.Values); i+=1 {
-		var (
-			zstr, sysstr string
-			ok bool
-		)
-		row := data.Values[i]
-		// if no sample defined, then we're done
-		if zstr, ok = row[sv.ZSampleColumn].(string); !ok || len(zstr)==0 {
+	for i := sv.HeaderRow + 1; i < sheet.Rows(); i++ {
+		if len(sheet.Get(i, sv.ZSampleColumn)) == 0 {
 			break
 		}
-		nzsamples += 1
+		nzsamples++
 
-		hasSysName := false
 		hasSysCount := false
 		hasMaxDistance := false
 
-		// we have a ZSample defined, check sysname
-		if sysstr, ok = row[sv.SysNameColumn].(string); ok && len(sysstr)>=0 {
-			hasSysName = true
-		}
-		if syscount, err := strconv.Atoi(row[sv.SystemCountColumn].(string)); err == nil &&
+		if syscount, err := strconv.Atoi(
+			sheet.Get(i, sv.SystemCountColumn)); err == nil &&
 			syscount >= 0 && syscount < 50 {
 			hasSysCount = true
 		}
-		if len(row) > sv.MaxDistanceColumn {
-			if maxdst, err := strconv.ParseFloat(row[sv.MaxDistanceColumn].(string), 32); err == nil && maxdst >= 0 && maxdst <= 20 {
-				hasMaxDistance = true
-			}
+		if maxdst, err := strconv.ParseFloat(
+			sheet.Get(i, sv.MaxDistanceColumn), 32); err == nil &&
+			maxdst >= 0 && maxdst <= 20 {
+			hasMaxDistance = true
 		}
 
-		if hasSysName && ( hasSysCount || hasMaxDistance ) {
-			nsamples += 1
+		if hasSysCount || hasMaxDistance {
+			nsamples++
 		}
 	}
-	return float32(nzsamples) * sv.MinSampleRatio < float32(nsamples)
+	return float32(nzsamples)*sv.MinSampleRatio < float32(nsamples)
 }
